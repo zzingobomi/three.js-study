9550853663fe7f74f09df2802304b39c51acda66
zzingobomi <zzingo5@naver.com>
2022-03-26 07:12:43 +0900

    10-glft file

--- 01-basic.js
+++ 01-basic.js
@@ -1,77 +1,81 @@
 import * as THREE from "../build/three.module.js";
+import { OrbitControls } from "../examples/jsm/controls/OrbitControls.js";
 
 class App {
-	constructor() {
-		const divContainer = document.querySelector("#webgl-container");
-		this._divContainer = divContainer;
+  constructor() {
+    const divContainer = document.querySelector("#webgl-container");
+    this._divContainer = divContainer;
 
-		const renderer = new THREE.WebGLRenderer({ antialias: true });
-		renderer.setPixelRatio(window.devicePixelRatio);
-		divContainer.appendChild(renderer.domElement);
-		this._renderer = renderer;
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    divContainer.appendChild(renderer.domElement);
+    this._renderer = renderer;
 
-		const scene = new THREE.Scene();
-		this._scene = scene;
+    const scene = new THREE.Scene();
+    this._scene = scene;
 
-		this._setupCamera();
-		this._setupLight();
-		this._setupModel();
+    this._setupCamera();
+    this._setupLight();
+    this._setupModel();
+    this._setupControls();
 
-		window.onresize = this.resize.bind(this);
-		this.resize();
+    window.onresize = this.resize.bind(this);
+    this.resize();
 
-		requestAnimationFrame(this.render.bind(this));
-	}
+    requestAnimationFrame(this.render.bind(this));
+  }
 
-	_setupCamera() {
-		const width = this._divContainer.clientWidth;
-		const height = this._divContainer.clientHeight;
-		const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-		camera.position.z = 2;
-		this._camera = camera;
-	}
+  _setupControls() {
+    new OrbitControls(this._camera, this._divContainer);
+  }
 
-	_setupLight() {
-		const color = 0xffffff;
-		const intensity = 1;
-		const light = new THREE.DirectionalLight(color, intensity);
-		light.position.set(-1, 2, 4);
-		this._scene.add(light);
-	}
+  _setupCamera() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+    camera.position.z = 2;
+    this._camera = camera;
+  }
 
-	_setupModel() {
-		const geometry = new THREE.BoxGeometry(1, 1, 1);
-		const meterial = new THREE.MeshPhongMaterial({ color: 0x44a88 });
+  _setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    this._scene.add(light);
+  }
 
-		const cube = new THREE.Mesh(geometry, meterial);
+  _setupModel() {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const meterial = new THREE.MeshPhongMaterial({ color: 0x44a88 });
 
-		this._scene.add(cube);
-		this._cube = cube;
-	}
+    const cube = new THREE.Mesh(geometry, meterial);
 
-	resize() {
-		const width = this._divContainer.clientWidth;
-		const height = this._divContainer.clientHeight;
+    this._scene.add(cube);
+    this._cube = cube;
+  }
 
-		this._camera.aspect = width / height;
-		this._camera.updateProjectionMatrix();
+  resize() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
 
-		this._renderer.setSize(width, height);
-	}
+    this._camera.aspect = width / height;
+    this._camera.updateProjectionMatrix();
 
-	render(time) {
-		this._renderer.render(this._scene, this._camera);
-		this.update(time);
-		requestAnimationFrame(this.render.bind(this));
-	}
+    this._renderer.setSize(width, height);
+  }
 
-	update(time) {
-		time *= 0.001; // second unit
-		this._cube.rotation.x = time;
-		this._cube.rotation.y = time;
-	}
+  render(time) {
+    this._renderer.render(this._scene, this._camera);
+    this.update(time);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  update(time) {
+    time *= 0.001; // second unit
+  }
 }
 
 window.onload = function () {
-	new App();
+  new App();
 };
---
+++ 09-webcam.js
@@ -0,0 +1,110 @@
+import * as THREE from "../build/three.module.js";
+import { OrbitControls } from "../examples/jsm/controls/OrbitControls.js";
+
+class App {
+  constructor() {
+    const divContainer = document.querySelector("#webgl-container");
+    this._divContainer = divContainer;
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    divContainer.appendChild(renderer.domElement);
+    this._renderer = renderer;
+
+    const scene = new THREE.Scene();
+    this._scene = scene;
+
+    this._setupCamera();
+    this._setupLight();
+    this._setupVideo();
+    this._setupControls();
+
+    window.onresize = this.resize.bind(this);
+    this.resize();
+
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  _setupVideo() {
+    const video = document.createElement("video");
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const constraints = {
+        video: { width: 1280, height: 720 },
+      };
+
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then((stream) => {
+          video.srcObject = stream;
+          video.play();
+
+          const videoTexture = new THREE.VideoTexture(video);
+          this._videoTexture = videoTexture;
+
+          this._setupModel();
+        })
+        .catch(function (error) {
+          console.error("카메라에 접근할 수 없습니다.", error);
+        });
+    } else {
+      console.error("MediaDevices 인터페이스 사용 불가");
+    }
+  }
+
+  _setupControls() {
+    new OrbitControls(this._camera, this._divContainer);
+  }
+
+  _setupCamera() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+    camera.position.z = 2;
+    this._camera = camera;
+  }
+
+  _setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    this._scene.add(light);
+  }
+
+  _setupModel() {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const meterial = new THREE.MeshPhongMaterial({
+      map: this._videoTexture,
+    });
+
+    const cube = new THREE.Mesh(geometry, meterial);
+
+    this._scene.add(cube);
+    this._cube = cube;
+  }
+
+  resize() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+
+    this._camera.aspect = width / height;
+    this._camera.updateProjectionMatrix();
+
+    this._renderer.setSize(width, height);
+  }
+
+  render(time) {
+    this._renderer.render(this._scene, this._camera);
+    this.update(time);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  update(time) {
+    time *= 0.001; // second unit
+  }
+}
+
+window.onload = function () {
+  new App();
+};
